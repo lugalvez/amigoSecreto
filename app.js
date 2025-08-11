fbf3cc8d2690d7fff10e69032d9f692844db1a32
Luis Gálvez <galvezluis1983@gmail.com>
2025-08-11 11:09:03 -0400

    Primera prueba

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let  amigo = "";
+let  amigos = [];
+let  lista = []
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+function agregarAmigos() {
+    let amigo = document.getElementById('amigo');
+     if (amigoInterno == "") {
+        alert ("Por favor, inserte un nombre.")
+    } else {
+         amigos.push(amigoInterno);
+        document.querySelector('amigo').value = '';
+    }
+    return;
+}
+function actualizaAmigos() {
+    lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""
+    for (let index = 0; index < amigos.length; index++) {
+       const nuevoElemento = document.createElement("li");
+           nuevoElemento.textContent = amigos[index];
+           lista.appendChild(nuevoElemento);
+    }
+    
+    return;
+}
+function generarSorteo() {
+    if (amigos.length <> 0) {
+        let numeroGenerado =  Math.floor(Math.random()*amigos.length);
+        let elemento = document.getElementById("resultado");
+        asignarTextoElemento(elemento, amigos[numeroGenerado])
+     }
